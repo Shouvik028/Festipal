d78d576abdfc492aaf7e07ac6e03814599b18bfe
slimshadeeyy <67092891+slimshadeeyy@users.noreply.github.com>
2024-04-03 23:02:05 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -25,7 +25,7 @@
                 <div class="col-md-6 contacts">
                     <h1 style="color:#000080 ; font-size:42px ; font-style:bold "><span class="glyphicon glyphicon-user"></span> College</h1>
                     <p>
-                        <span class="glyphicon glyphicon-envelope"></span> Email: collegeevent@gmail.com<br>
+                        <span class="glyphicon glyphicon-envelope"></span> Email: festipal@gmail.com<br>
     
                         <span class="glyphicon glyphicon-phone"></span> Mobile: 9123456789
                     </p>
