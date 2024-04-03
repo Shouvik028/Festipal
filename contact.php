7f8e5f5806477290d6e2231ace9a373ee1271f47
Piyush Prakash <107531178+DemoL2004@users.noreply.github.com>
2024-04-04 00:02:49 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -30,14 +30,6 @@
                         <span class="glyphicon glyphicon-phone"></span> Mobile: 9123456789
                     </p>
                 </div>
-                <div class="col-md-6 contacts">
-                    <h1 style="color:#000080 ; font-size:42px ; font-style:bold "><span class="glyphicon glyphicon-user"></span> Example</h1>
-                    <p>
-                        <span class="glyphicon glyphicon-envelope"></span> Email: example@gmail.com<br>
-    
-                        <span class="glyphicon glyphicon-phone"></span> Mobile: 9876543210
-                    </p>
-                </div>
             </div>
 			
             
