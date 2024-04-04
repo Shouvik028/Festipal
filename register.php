03e2a1c4f32654eceab68b34539b6c337e692699
DemoL <pforpiyush1234@gmail.com>
2024-04-04 12:53:03 +0530

    made the container smaller in register.php and centered the input fields

--- register.php
+++ register.php
@@ -11,30 +11,30 @@
     <?php require 'utils/header.php'; ?>
     <div class = "content" style="background-image:url('https://slcm.manipal.edu/assets/images/mu_1.jpg');background-size: cover;"><!--body content holder-->
     <br>
-    <div class = "container" style="background-color:rgba(255, 255, 255, 0.7);border:1px solid grey; border-radius:50px;padding-bottom:30px;">
-                <div class ="col-md-6 col-md-offset-3" style="margin-top:20px;>
+    <div class = "container" style="background-color:rgba(255, 255, 255, 0.7);border:1px solid grey; border-radius:50px;padding-bottom:30px;margin:0 auto;max-width:600px;">
+                
     <form method="POST">
-
+        <br>
    
         <label>Student RegNo:</label><br>
-        <input type="text" name="usn" class="form-control" required><br><br>
+        <input type="text" name="usn" class="form-control" required style="max-width:550px;margin:0 auto;"><br><br>
 
         <label>Student Name:</label><br>
-        <input type="text" name="name" class="form-control" required><br><br>
+        <input type="text" name="name" class="form-control" required style="max-width:550px;margin:0 auto;"><br><br>
 
         <label>Branch:</label><br>
-        <input type="text" name="branch" class="form-control" required><br><br>
+        <input type="text" name="branch" class="form-control" required style="max-width:550px;margin:0 auto;"><br><br>
 
         <label>Semester:</label><br>
-        <input type="text" name="sem" class="form-control" required><br><br>
+        <input type="text" name="sem" class="form-control" required style="max-width:550px;margin:0 auto;"><br><br>
 
         <label>Phone:</label><br>
-        <input type="text" name="phone"  class="form-control" required><br><br>
-
-        <button type="submit" name="update" required>Submit</button><br><br>
+        <input type="text" name="phone"  class="form-control" required style="max-width:550px;margin:0 auto;"><br><br>
+        
+        <button type="submit" name="update" required style="">Submit</button><br><br>
         <a href="usn.php" ><u>Already registered ?</u></a>
 
-    </div>
+    
     </div>
     
     </form>
